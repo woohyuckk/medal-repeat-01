db675f36f6dc443bf148091257c8d1d2a33ce62a
woohyuckk <woohyuckk@gmail.com>
2025-01-24 22:05:27 +0900

    feat: 금메달 순 정렬

--- src/components/MedalList.jsx
+++ src/components/MedalList.jsx
@@ -1,46 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 const medalListBtn = {
-    backgroundColor: "transparent",
-    border: "0",
-    cursor: "pointer",
-    marginLeft: "5px",
-  };
-  
+  backgroundColor: "transparent",
+  border: "0",
+  cursor: "pointer",
+  margin : "0"
+};
 
 const MedalList = ({ medalRecords, setMedalRecords }) => {
-  
-  
-  const handleGoldSort = () =>{
-    const sortedRecords = medalRecords.sort((a,b)=>{ 
-        if(b.goldMedal !== a.goldMedal){
-            return 
-            b.goldMedal-a.goldMedal
-        }
-        } )
-  }
-  
-  
-    const handleDelete = (nation) => {
+
+  const [isGoldDescending, setIsGoldDescending] = useState(true); // 금메달 정렬 상태
+
+  const handleGoldSort = () => {
+    const sortedRecords = medalRecords.sort((a, b) => {
+      if (b.goldMedal !== a.goldMedal) {
+        return isGoldDescending
+          ? b.goldMedal - a.goldMedal
+          : a.goldMedal - b.goldMedal;
+      }
+      return isGoldDescending
+        ? b.goldMedal - a.goldMedal
+        : a.goldMedal - b.goldMedal;
+    });
+    setMedalRecords(sortedRecords);
+    setIsGoldDescending(!isGoldDescending);
+  };
+
+  const handleDelete = (nation) => {
     const deletedMedalRecords = medalRecords.filter((record) => {
       return record.nation !== nation;
     });
     //  업데이트하면서 금메달 순으로 정렬할까 말까!!!!!!
-    setMedalRecords(deletedMedalRecords.sort((a,b)=>b.goldMedal-a.goldMedal));
+    setMedalRecords(
+      deletedMedalRecords.sort((a, b) => b.goldMedal - a.goldMedal)
+    );
   };
 
-  
-
-
-
   return (
-    <div>
-      <table>
+    <div className="table-container">
+      <table >
         <thead>
           <tr>
             <th>국가명</th>
-            <th>금메달
-                <button style={medalListBtn} onClick={handleGoldSort}>{isGoldDescending? "▼": "▲"}</button>
+            <th>
+              금메달
+              <button style={medalListBtn} onClick={handleGoldSort}>
+                {isGoldDescending ? "▼" : "▲"}
+              </button>
             </th>
             <th>은메달</th>
             <th>동메달</th>
@@ -57,7 +63,7 @@ const MedalList = ({ medalRecords, setMedalRecords }) => {
               <td>{record.bronzeMedal}</td>
               <td>{record.sumMedals}</td>
               <td>
-                <button onClick={() => handleDelete(record.nation)}>
+                <button className="button.delete-button" onClick={() => handleDelete(record.nation)}>
                   삭제
                 </button>
               </td>
