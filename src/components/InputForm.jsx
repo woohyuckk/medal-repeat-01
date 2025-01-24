7075b8876b21309284abe9e5bc7ebb664dcc1af6
woohyuckk <woohyuckk@gmail.com>
2025-01-24 15:01:33 +0900

    feat : InputForm 레이아웃 구성

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputForm = () => {
+  const [nation, setNation] = useState("");
+  const [goldMedal, setgoldMedal] = useState(0);
+  const [silverMedal, setsilverMedal] = useState(0);
+  const [bronzeMedal, setbronzeMedal] = useState(0);
+
   return (
     <div>
-      <label></label>
-      <input type="text"></input>
-
-      <label></label>
-      <input type="number"></input>
-
-      <label></label>
-      <input type="number"></input>
-      
-      <label></label>
-      <input type="number"></input>
+      <label for="국가명">국가명</label><br/>
+      <input
+        id="국가명"
+        type="text"
+        value={nation}
+        onChange={(e) => {
+          setNation(e.target.value.trim);
+        }}
+      ></input>
+      <label for="금메달">금메달</label><br/>
+      <input
+        id="금메달"
+        type="number"
+        value={goldMedal}
+        onChange={(e) => {
+          setgoldMedal(e.target.value.trim);
+        }}
+      ></input>
+      <label for="은메달">은메달</label><br/>
+      <input
+        id="은메달"
+        type="number"
+        value={silverMedal}
+        onChange={(e) => {
+          setsilverMedal(e.target.value.trim);
+        }}
+      ></input>
+      <label for="동메달">동메달</label><br/>
+      <input
+        id="동메달"
+        type="number"
+        value={goldMedal}
+        onChange={(e) => {
+          setgoldMedal(e.target.value.trim);
+        }}
+      ></input>
     </div>
   );
 };
